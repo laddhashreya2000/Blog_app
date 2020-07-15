3742ceda04480a06d3ac85ccd46a4fd18c611f74
Shreya <2000sladdha@gmail.com>
2020-07-15 18:49:28 +0530

    Modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,22 +20,22 @@ import { UserData } from './providers/user-data';
 export class AppComponent implements OnInit {
   appPages = [
     {
-      title: 'Schedule',
+      title: 'Feed',
       url: '/app/tabs/schedule',
       icon: 'calendar'
     },
     {
-      title: 'Speakers',
+      title: 'People',
       url: '/app/tabs/speakers',
       icon: 'people'
     },
+    // {
+    //   title: 'Map',
+    //   url: '/app/tabs/map',
+    //   icon: 'map'
+    // },
     {
-      title: 'Map',
-      url: '/app/tabs/map',
-      icon: 'map'
-    },
-    {
-      title: 'About',
+      title: 'My Blog',
       url: '/app/tabs/about',
       icon: 'information-circle'
     }
